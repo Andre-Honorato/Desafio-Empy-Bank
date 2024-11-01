f44c63fc57a3189645eeb7cf402ceb7934db635b
André Honorato <gamera103@gmail.com>
2024-11-01 11:42:25 -0300

    Default port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 import { assistantRoutes, clientRoutes } from './routes'
 
 const app: Express = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
